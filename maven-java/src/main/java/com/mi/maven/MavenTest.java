d12cff0cbc9dd6b3e5d2ea7e5e5d498817354d32
xiaomi <1910375442@qq.com>
2019-09-27 11:24:38 +0800

    添加几个文件

---
+++ maven-java/src/main/java/com/mi/maven/MavenTest.java
@@ -0,0 +1,13 @@
+package com.mi.maven;
+
+/**
+ * @version V1.0
+ * @author: XiaoMi
+ * @date: 2019/9/27 10:12
+ * @description:
+ */
+public class MavenTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
